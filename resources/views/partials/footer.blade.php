ce9bbd622d51205246d9a4df8158b1c7aa5ab81a
Carlos Pereda <carlonchomaster@gmail.com>
2021-03-25 17:34:12 -0500

    Se mejoro los iconos, se necesita probar otro plugin de formulario

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,16 +5,16 @@
             <p class="column is-two-thirds">&copy;Frontent:100p Copyright {{ date('Y') }} | Todos los derecho reservados | <a href="#">Politica de Privacidad</a></p>
             <div class="social-networks column">
                 <a class="social-network" href="">
-                    @svg('icon-whatsapp', 'network-whatsapp')
+                    @svg('whatsapp-square-brands', 'network-whatsapp')
                 </a>
                 <a class="social-network" href="https://www.linkedin.com/in/carlos-pereda" target="_blank">
-                    @svg('icon-linkedin', 'network-linkedin')
+                    @svg('linkedin-brands', 'network-linkedin')
                 </a>
                 <a class="social-network" href="https://www.facebook.com/carlospered4" target="_blank">
-                    @svg('icon-facebook', 'network-facebook')
+                    @svg('facebook-square-brands', 'network-facebook')
                 </a>
                 <a class="social-network" href="https://www.youtube.com/c/CarlonchoMaster" target="_blank">
-                    @svg('icon-youtube', 'network-youtube')
+                    @svg('youtube-square-brands', 'network-youtube')
                 </a>
             </div>
         </div>
